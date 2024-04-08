3a8c232f200a0d653bd847a17f5900d06634fcd4
z.esartia23@itstep.ge Jmuxapro123 <zaal11@gmail.com config --global user.name Zaaligit config --global user.email zaal11@gmail.com>
2024-04-08 11:51:53 +0400

    This is Start of My project lets see what we got

---
+++ Javascript/data.js
@@ -0,0 +1,57 @@
+export const bookFlight = [
+  {
+    image: "./img/image1.png",
+    title: "Book Flight",
+    paragraph:
+      "book memorable journey with our flight booking services! we're thrilled to present you flight options to suit your travel needs.",
+  },
+  {
+    image: "./img/image2.png",
+    title: "Book Hotels",
+    paragraph:
+      "book memorable journey with our flight booking services! we're thrilled to present you flight options to suit your travel needs.",
+  },
+  {
+    image: "./img/image3.png",
+    title: "Book Cruises",
+    paragraph:
+      "book memorable journey with our flight booking services! we're thrilled to present you flight options to suit your travel needs.",
+  },
+  {
+    image: "./img/image4.png",
+    title: "Book family Vacation",
+    paragraph:
+      "book memorable journey with our flight booking services! we're thrilled to present you flight options to suit your travel needs.",
+  },
+];
+
+export const tourOffers = [
+  {
+    id: 1,
+    image: "./img/image_1.png",
+    title: "Barcelona Spain",
+    paragraph: "2 adults 7days",
+    price: "2400$",
+  },
+  {
+    id: 2,
+    image: "./img/image_2.png",
+    title: "Rome Italy",
+    paragraph: "2 adults 3days",
+    price: "1800$",
+  },
+  {
+    id: 3,
+    image: "./img/image_3.png",
+    title: "Paris France",
+    paragraph: "2 adults 4days",
+    price: "2100$",
+  },
+  {
+    id: 4,
+    image: "./img/image_4.png",
+    title: "Luxor City, Egypt",
+    paragraph: "2 adults 3days",
+    price: "800$",
+  },
+];
