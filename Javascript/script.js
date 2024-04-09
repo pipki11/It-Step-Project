71dc64941b2bb67c7e3e1195f3d7ebd2812fc286
z.esartia23@itstep.ge Jmuxapro123 <zaal11@gmail.com config --global user.name Zaaligit config --global user.email zaal11@gmail.com>
2024-04-09 13:31:47 +0400

    From now i am starting to work with Javascript and when i press user btn the three sign option we get

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,33 +1,6 @@
-import { bookFlight } from "./data.js";
-import { tourOffers } from "./data.js";
+const signBtn = document.querySelector(".fa-user");
+const loginInPage = document.querySelector(".login");
 
-const area = document.querySelector(".test");
-const secondArea = document.querySelector(".test1");
-
-document.addEventListener("DOMContentLoaded", function () {
-  for (let i = 0; i < bookFlight.length; i++) {
-    area.innerHTML += `
-    <div class = 'card'>
-    <img src = '${bookFlight[i].image}' />    
-     <h1> ${bookFlight[i].title} </h1>
-     <p>  ${bookFlight[i].paragraph} </p>
-     </div>
-    `;
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  for (let i = 0; i < tourOffers.length; i++) {
-    secondArea.innerHTML += `
-    <div class = 'card1'>
-    <img src = '${tourOffers[i].image}' />  
-    <h3> ${tourOffers[i].title} </h3>  
-    <p> ${tourOffers[i].paragraph} </p>
-    <p> ${tourOffers[i].price} </p>
-     </div>
-     
-     
-     
-    `;
-  }
+signBtn.addEventListener("click", function () {
+  loginInPage.classList.toggle("loginShow");
 });
